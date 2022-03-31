34ea9fac7e227bc6da2cf4a8df98a5605787ccd9
nikhil arora <narora200@gmail.com>
2022-03-31 18:00:27 +0530

    feat: need for multiple threads

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use std::{fs, thread};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -15,8 +16,13 @@ fn handle_stream(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let get = b"GET / HTTP/1.1\r\n";
-    let (status_line, filename) = if buffer.starts_with(get) {
+    let get_request = b"GET / HTTP/1.1\r\n";
+    let sleep_request = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get_request) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else if buffer.starts_with(sleep_request) {
+        thread::sleep(Duration::from_secs(10));
         ("HTTP/1.1 200 OK", "index.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
