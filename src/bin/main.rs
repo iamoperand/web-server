d9fe261035d465f1753ae7b745b91ec49b5cb705
nikhil arora <narora200@gmail.com>
2022-04-02 00:52:51 +0530

    feat: multi-threaded server

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,12 +3,18 @@ use std::net::{TcpListener, TcpStream};
 use std::time::Duration;
 use std::{fs, thread};
 
+use web_server::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_stream(stream);
+        pool.execute(|| {
+            handle_stream(stream);
+        });
     }
 }
 
