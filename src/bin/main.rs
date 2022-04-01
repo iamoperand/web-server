b5bb8f869ed5a053e16fe311d5e17f5042a1140a
nikhil arora <narora200@gmail.com>
2022-04-02 01:50:29 +0530

    feat: graceful shutdown and cleanup

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -10,12 +10,14 @@ fn main() {
 
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
         pool.execute(|| {
             handle_stream(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_stream(mut stream: TcpStream) {
